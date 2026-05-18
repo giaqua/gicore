e3cc0b6c4d87c4143be1c6573fbca481a5a3c6cb
hamzahabdo <alhamza1997@gmail.com>
2026-05-18 13:40:59 +0300

    Create NEw Module and new business letter doctypes

---
+++ gicore/relations/doctype/business_letter/business_letter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, GI Aqua Tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Business Letter", {
+// 	refresh(frm) {
+
+// 	},
+// });
