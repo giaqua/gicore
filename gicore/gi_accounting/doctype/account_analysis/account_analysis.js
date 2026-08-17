401079a5f7b634aa085d0243bd76a1b666978971
hamzahabdo <alhamza1997@gmail.com>
2026-08-17 10:05:20 +0300

    build doctype for account analysis

---
+++ gicore/gi_accounting/doctype/account_analysis/account_analysis.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, GI Aqua Tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Account Analysis", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ gicore/gi_accounting/doctype/account_analysis/account_analysis.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, GI Aqua Tech and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class AccountAnalysis(NestedSet):
+	pass
