bae136f087fa9a3980168dd8c68bcd92e20c4da8
hamzahabdo <alhamza1997@gmail.com>
2026-09-06 14:02:24 +0300

    add child doctype for project allocation

---
+++ gicore/gi_accounting/doctype/hm_project_allocation_settings/test_hm_project_allocation_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, GI Aqua Tech and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHMProjectAllocationSettings(FrappeTestCase):
+	pass
---
+++ gicore/gi_accounting/doctype/hm_project_allocation_target/hm_project_allocation_target.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, GI Aqua Tech and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class HMProjectAllocationTarget(Document):
+	pass
