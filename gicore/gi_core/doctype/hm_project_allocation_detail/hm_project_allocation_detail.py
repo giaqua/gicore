fd117a2085bd1da437acc13ba0341953be90b5eb
hamzahabdo <alhamza1997@gmail.com>
2026-09-06 14:04:10 +0300

    remove doctype

--- gicore/gi_core/doctype/hm_project_allocation_detail/hm_project_allocation_detail.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, GI Aqua Tech and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class HMProjectAllocationDetail(Document):
-	pass
